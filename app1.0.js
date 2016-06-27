8367f74cd7dcb71bdcb56e2d1ca7a3f66bc9a822
nat1994 <nathanaelchan94@gmail.com>
2016-06-27 19:05:03 +0800

    Add files via upload

---
+++ app1.0.js
@@ -0,0 +1,63 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+      // Don't remove this line unless you know what you are doing. It stops the viewport
+      // from snapping when text inputs are focused. Ionic handles this internally for
+      // a much nicer keyboard experience.
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+function party4life(){
+  this.checkSetup();
+
+  this.search = document.getElementById('search');
+
+  this.search.addEventListener('click', this.SearchButton. bind(this));
+
+  this.initFirebase();
+}
+
+party4life.prototype.initFirebase = function(){
+  this.auth = firebase.auth();
+  this.database = firebase.database();
+  this.storage = firebase.storage();
+  this.auth.onAuthStateChanged(this.onAuthStateChanged.bind(this));
+};
+
+party4life.prototype.SearchButton = function(){
+  var ref = new Firebase("https://foodmusiclaughter.firebaseio.com");
+  ref.orderByChild("height").on("child_added", function(snapshot) {
+  console.log(snapshot.key() + " was " + snapshot.val().height + " meters tall");
+});
+
+party4life.prototype.checkSetup = function(){
+  if (!window.firebase || !(firebase.app instanceof Function) || !window.config) {
+    window.alert('You have not configured and imported the Firebase SDK. ' +
+        'Make sure you go through the codelab setup instructions.');
+  } else if (config.storageBucket === '') {
+    window.alert('Your Firebase Storage bucket has not been enabled. Sorry about that. This is ' +
+        'actually a Firebase bug that occurs rarely.' +
+        'Please go and re-generate the Firebase initialisation snippet (step 4 of the codelab) ' +
+        'and make sure the storageBucket attribute is not empty.');
+  }
+};
+
+window.onload = function(){
+  window.party = new party4life();
+}
